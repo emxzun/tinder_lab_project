15971a01c216d6593b0a99d31e6b3f5c2a0ba34d
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2023-03-15 18:00:07 +0600

    fix: model likedislike

--- applications/likedislike/models.py
+++ applications/likedislike/models.py
@@ -5,7 +5,6 @@ from applications.account.models import Profile
 
 
 class LikeDislike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     like = models.BooleanField(default=False)
     dislike = models.BooleanField(default=False)
