3205b6b6e8baac88e1937eb38ea7ee4497ad3da9
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2023-03-15 17:32:58 +0600

    Fix: change name likedislike

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger')),
     path('api/v1/account/', include('applications.account.urls')),
     path('api/v1/recommendations/', include('applications.recommendations.urls')),
-    path('api/v1/likes/', include('applications.likes.urls'))
+    path('api/v1/likedislike/', include('applications.likedislike.urls'))
 ]
 
 if settings.DEBUG:
