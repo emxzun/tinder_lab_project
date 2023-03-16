1023a653e5253c367c1a1b0ea36a71016ec7e498
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2023-03-16 08:18:09 +0600

    settings DB set

--- config/settings.py
+++ config/settings.py
@@ -92,10 +92,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DB_URL'), conn_max_age=600),
 }
 
 
