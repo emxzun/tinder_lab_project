9729677c5f762a33db58d8932e57b706ead36869
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2023-03-16 22:29:03 +0600

    Fix: model like

--- applications/likedislike/serializers.py
+++ applications/likedislike/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from applications.likedislike.models import LikeDislike
 
 class LikeDislikeSerializer(serializers.Serializer):
-    who_user_liked_id = serializers.ImageField()
-    whom_user_liked_id = serializers.ImageField()
-    like = serializers.BooleanField()
-    dislike = serializers.BooleanField()
+
+    class Meta:
+        model = LikeDislike
+        fields = '__all__'
+    
