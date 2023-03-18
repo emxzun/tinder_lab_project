8731de867b950d8006195288ce2e640378b5c863
emir <e352709@gmail.com>
2023-03-19 01:02:05 +0600

    commit

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
+if [ $CREATE_SUPERUSER ];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python manage.py createsuperuser --no-input
 fi
