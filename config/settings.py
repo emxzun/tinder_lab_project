5775c83acc7725bdd5a602810f2beb97541dda57
emir <e352709@gmail.com>
2023-03-23 21:41:57 +0600

    commit

--- config/settings.py
+++ config/settings.py
@@ -205,6 +205,6 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001"
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
